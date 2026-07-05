c89425101e30f17deee7128e7bb8e621ab5c760c
amirchuosho <68125062+amirchuosho@users.noreply.github.com>
2026-07-05 20:30:37 +0300

    cask: distribute v0.1.0 as DMG (frictionless via brew, quarantine stripped)

--- Casks/prosciutto.rb
+++ Casks/prosciutto.rb
@@ -1,8 +1,8 @@
 cask "prosciutto" do
   version "0.1.0"
-  sha256 "3b1e95cc9ec6be08fbd246a57c66e07f09d9b4f4eea28fd727d2f9e82fb3eadb"
+  sha256 "b9d154d1a92625e0d8957e3a2b4976ed4ea38a5f5d4afa0ede450fb91a160f2e"
 
-  url "https://github.com/amirchuosho/prosciutto/releases/download/v#{version}/Prosciutto-#{version}.zip",
+  url "https://github.com/amirchuosho/prosciutto/releases/download/v#{version}/Prosciutto-#{version}.dmg",
       verified: "github.com/amirchuosho/prosciutto/"
   name "Prosciutto"
   desc "Open-source visual clipboard manager for macOS"
